3eb75d01d32df8975442f255f05b77409e3f167d
TheAmlanSaikia <83667291+TheAmlanSaikia@users.noreply.github.com>
2022-10-17 16:17:45 +0530

    feat: initial commit

---
+++ src/Components/AddLegCard/AddLegCard.js
@@ -0,0 +1,13 @@
+const AddLegCard = ()=>{
+    return (
+        <div>
+           <div>Select Segments:</div>
+           <div>Buttons</div>
+           <div>Add Leg</div>
+       </div>
+    )
+}
+
+
+
+export { AddLegCard };
